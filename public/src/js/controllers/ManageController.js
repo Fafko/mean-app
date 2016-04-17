ecd60bc20a15d3374a02426c2afc9b4cafe29d84
Alex Panchenko <fafko93@gmail.com>
2016-04-17 12:21:35 +0300

    Binding product form with controller

--- public/src/js/controllers/ManageController.js
+++ public/src/js/controllers/ManageController.js
@@ -1,10 +1,43 @@
 (function(angular){
     "use strict";
 
-    angular.module('Controllers').controller('ManageController', ['$routeParams', 'ProductsService', 
-        function($routeParams, ProductsService){
+    angular.module('Controllers').controller('ManageController', ['$routeParams', 'Product', 'ErrorsHandlerService', '$rootScope',
+        function($routeParams, Product, ErrorsHandlerService, $rootScope){
             
         this.mode = $routeParams.id ? 'Update' : 'Create';
+            
+        
+            
+        var createProduct = function(_product, form){
+            $rootScope.$broadcast('spinner:show');
+
+            var  product = new Product();
+            
+            product.data = _product;
+            
+            product.$save();
+            
+        };
+            
+            
+        this.processProduct = function($event, product, form){
+            $event.preventDefault();
+            
+            var fields = [
+                'name',
+                'price',
+                'description'
+            ];
+            
+            ErrorsHandlerService.removeServerErrors(form, fields);
+
+            if(form.$valid) {
+                (this.mode == 'Create') ? 
+                    createProduct(product, form) : 
+                    updateProduct(product, form);
+            }
+            
+        }
             
     }]);
 
